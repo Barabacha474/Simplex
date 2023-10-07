a1ffb6dfd05542d41c42f708b8bfb2991623eac9
Saveliy Khlebnov <savel.khlebnov@yandex.ru>
2023-10-07 20:28:05 +0300

    refactor of code

--- simplex/models.py
+++ simplex/models.py
@@ -1,97 +1,98 @@
 class Matrix:
-    mat = []
+    _mat = list([])
+
+        
+    def __init__(self, arr = list([])):
+        self._mat = arr
 
     def mInput(self):
-        s = input()
-        while s != "":
-            row = [int(x) for x in s.split()]
-            self.mat.append(row)
-            s = input()
-        return self
+        while (s := input()) != "":
+            row = list(map(int, s.split())) 
+            self._mat.append(row)
+            
 
-    def multiplpyM(self, matrix):
+    def __mul__(self, matrix: 'Matrix') -> 'Matrix':
         result = [[sum(a * b for a, b in zip(self_row, matrix_col))
-                   for matrix_col in zip(*matrix)] for self_row in self.mat]
-        return result
+                   for matrix_col in zip(*matrix.getMatrix())] for self_row in self._mat]
+        return Matrix(result)
 
-    def inverseMatrix(self):
+    def inverseMatrix(self) -> 'Matrix':
         det = self.det()
 
-        if len(self.mat) == 2:
-            return [[self.mat[1][1] / det, -1 * self.mat[0][1] / det],
-                    [-1 * self.mat[1][0] / det, self.mat[0][0] / det]]
+        if len(self._mat) == 2:
+            return Matrix([[self._mat[1][1] / det, -1 * self._mat[0][1] / det],
+                    [-1 * self._mat[1][0] / det, self._mat[0][0] / det]])
 
         inv = Matrix()
-        for i in range(len(self.mat)):
+        for i in range(len(self._mat)):
             row = []
-            for j in range(len(self.mat)):
-                row.append(((-1) ** (i + j)) * self.det())
-            inv.mat.append(row)
-        inv.mat = inv.mTranspose()
-        matrix = inv.gerMatrix()
-        det = inv.det()
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                matrix[i][j] = matrix[i][j] / det
+            for j in range(len(self._mat)):
+                row.append(((-1) ** (i + j)) * self.minorMatrix(i, j).det())
+            inv._mat.append(row)
+        inv = inv.mTranspose()
+        matrix = inv.getMatrix()
+        
+        matrix = list(list(map(lambda x: x/det, row)) for row in matrix)
 
-        return matrix
+        return Matrix(matrix)
 
     def mTranspose(self):
-        return map(list, zip(*self.mat))
+        return Matrix(list(map(list, zip(*self._mat))))
 
-    def minorMatrix(self, i, j):
-        return [row[:j] + row[j + 1:] for row in (self.mat[:i] + self.mat[i + 1:])]
+    def minorMatrix(self, i: int, j: int) -> 'Matrix':
+        return Matrix([row[:j] + row[j + 1:] for row in (self._mat[:i] + self._mat[i + 1:])])
 
-    def det(self):
-        if len(self.mat) == 2:
-            return self.mat[0][0] * self.mat[1][1] - self.mat[0][1] * self.mat[1][0]
+    def det(self) -> float:
+        if len(self._mat) == 2:
+            return self._mat[0][0] * self._mat[1][1] - self._mat[0][1] * self._mat[1][0]
 
         minor = Matrix()
         det = 0
-        for i in range(len(self.mat)):
-            minor.mat = self.minorMatrix(0, i)
-            det += ((-1) ** i) * self.mat[0][i] * minor.det()
+        for i in range(len(self._mat)):
+            minor = self.minorMatrix(0, i)
+            det += ((-1) ** i) * self._mat[0][i] * minor.det()
         return det
 
-    def gerMatrix(self):
-        return self.mat
+    def getMatrix(self) -> list:
+        return self._mat
 
 
 class IdentityMatrix(Matrix):
     def __init__(self, len):
+        self._mat = [[0]*len]*len
         for i in range(len):
             for j in range(len):
                 if i == j:
-                    self.mat[i][j] = 1
-                else:
-                    self.mat[i][j] = 0
+                    self._mat[i][j] = 1
 
 
 class ZeroMatrix(Matrix):
     def __init__(self, len):
-        for i in range(len):
-            for j in range(len):
-                self.mat[i][j] = 0
+        self._mat = [[0]*len]*len
 
 
 class Vector:
-    v = []
+    _v = []
+
+    def __init__(self, arr = list()):
+        self._v = arr
+
 
     def vInput(self):
         s = input()
-        self.v = [int(x) for x in s.split()]
-        """For elimination of null string after vector"""
-        s = input()
-        return self
+        self._v = [int(x) for x in s.split()]
+        # For elimination of null string after vector
+        input()
 
-    def dot(self, v2):
+    def __mul__(self, v2: 'Vector') -> 'Vector':
         dot = 0
-        for i in range(len(v2)):
-            dot += self.v[i] * v2.v[i]
+        for i in range(len(v2.getVector())):
+            dot += self._v[i] * v2._v[i]
         return dot
 
-    def vTranspose(self):
-        return list(zip(*self.v))
+    def vTranspose(self) -> 'Vector':
+        return Vector(list(zip(*self._v)))
 
     def getVector(self):
-        return self.v
+        return self._v
+
